94d9eff042d2c60a1d841335d104cf1737dd8ec6
Sebastian Sliwinski <sebalaka@gmail.com>
2020-05-06 15:24:52 +0200

    end initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -82,26 +82,27 @@
       const thisProduct = this;
   
       /* find the clickable trigger (the element that should react to clicking) */
-  
+      const accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       /* START: click event listener to trigger */
-  
+      accordionTrigger.addEventListener('click', function(){
         /* prevent default action for event */
-  
+        event.preventDefault();
         /* toggle active class on element of thisProduct */
-  
+        thisProduct.element.classList.toggle('active');
         /* find all active products */
-  
+        const activeProducts = document.querySelectorAll('.active');
         /* START LOOP: for each active product */
-  
+        for (let activeProduct of activeProducts) {
           /* START: if the active product isn't the element of thisProduct */
-  
+          if (activeProduct != thisProduct.element) {
             /* remove class active for the active product */
-  
+            activeProduct.classList.remove('active');
           /* END: if the active product isn't the element of thisProduct */
-  
+          }
         /* END LOOP: for each active product */
-  
-      /* END: click event listener to trigger */
+        }
+      /* END: click event listener to trigger */ 
+      });
     }
   }
   
