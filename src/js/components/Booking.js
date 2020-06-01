7152aa1735fd87c8d5675b1f1b5e50eec10dc42f
Sebastian Sliwinski <sebalaka@gmail.com>
2020-06-01 16:29:24 +0200

    zad 10.3 end

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -4,6 +4,7 @@ import {
 } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import { DatePicker } from './DatePicker.js';
+import { HourPicker } from './HourPicker.js';
 
 export class Booking {
   constructor(element) {
@@ -22,6 +23,7 @@ export class Booking {
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets() {
@@ -29,6 +31,7 @@ export class Booking {
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
     console.log('date', thisBooking.datePicker);
   }
 }
