2a8dc142eac960b892e8dc9fa552154b7f94cf87
Sebastian Sliwinski <sebalaka@gmail.com>
2020-06-03 09:43:57 +0200

    zad 11.1

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,16 +1,77 @@
 import {
   templates,
-  select
+  select,
+  settings
 } from '../settings.js';
+import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
-import { DatePicker } from './DatePicker.js';
-import { HourPicker } from './HourPicker.js';
+import {
+  DatePicker
+} from './DatePicker.js';
+import {
+  HourPicker
+} from './HourPicker.js';
 
 export class Booking {
   constructor(element) {
     const thisBooking = this;
     thisBooking.render(element);
     thisBooking.initWidgets();
+    thisBooking.getData();
+  }
+
+  getData() {
+    const thisBooking = this;
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      booking: [
+        startDateParam,
+        endDateParam,
+      ],
+
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
+
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+
+    };
+
+    // console.log('getData params', params)
+
+    const urls = {
+      booking: settings.db.url + '/' + settings.db.booking + '?' + params.booking.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.event + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat: settings.db.url + '/' + settings.db.event + '?' + params.eventsRepeat.join('&'),
+    };
+
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function (allResponses) {
+        const bookingResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all([
+          bookingResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function ([bookings, eventsCurrent, eventsRepeat]) {
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });
   }
 
   render(element) {
